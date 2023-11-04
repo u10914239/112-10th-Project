0e2a87221895292a041f3cdcbb89b3ce5f73203d
DESKTOP-89BTP30\Sam <samluxian@gmail.com>
2023-11-04 21:32:48 +0800

    slime Hit Fixed 2

--- Assets/Scripts/Movements/mainAttack.cs
+++ Assets/Scripts/Movements/mainAttack.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class mainAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public bool dealtDamage;
-    public static bool inRange;
-
+    
     public Animator anim;
     public AudioSource Punch;
+
     void Start()
     {
-        dealtDamage = false;
+        
     }
 
     // Update is called once per frame
@@ -21,36 +19,18 @@ public class mainAttack : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             Attack();
-            Punch.Play();
-            if (inRange && dealtDamage)
-            {
-                FollowEnemy.isAttacked = true;
-                mainCharactor.magicNumber +=1;
-                dealtDamage = false;
-            }
+            
+            
         }
         
     }
     void Attack()
     {
-        dealtDamage = true;
-        anim.SetTrigger("Attack");
-
        
+        anim.SetTrigger("Attack");
+        Punch.Play();
+
     }
 
-    void OnTriggerStay(Collider Hit)
-    {
-        if(Hit.gameObject.tag == ("Enemy"))
-        {
-            inRange = true;
-        }
-    }
-    void OnTriggerExit(Collider Hit)
-    {
-        if(Hit.gameObject.tag == ("Enemy"))
-        {
-            inRange = false;
-        }
-    }
+    
 }
--- Assets/Scripts/Movements/secondAttack.cs
+++ Assets/Scripts/Movements/secondAttack.cs
@@ -5,53 +5,36 @@ using UnityEngine;
 public class secondAttack : MonoBehaviour
 {
     // Start is called before the first frame update
-    public bool dealtDamage;
-    public static bool inRange2;
+    
 
     public Animator anim;
     public AudioSource Punch;
     void Start()
     {
-        dealtDamage = false;
+        
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Joystick1Button5))
         {
+            
             Attack();
-            Punch.Play();
-            if (inRange2 && dealtDamage)
-            {
-                FollowEnemy.isAttacked = true;
-                secondCharactor.magicNumber +=1;
-                dealtDamage = false;
-            }
+            secondCharactor.magicNumber +=1;
+            
+            
         }
         
     }
 
     void Attack()
     {
-        dealtDamage = true;
+        
         anim.SetTrigger("Attack");
-
+        Punch.Play();
 
     }
 
-    void OnTriggerStay(Collider Hit)
-    {
-        if(Hit.gameObject.tag == ("Enemy"))
-        {
-            inRange2 = true;
-        }
-    }
-    void OnTriggerExit(Collider Hit)
-    {
-        if(Hit.gameObject.tag == ("Enemy"))
-        {
-            inRange2 = false;
-        }
-    }
+    
 
 
 }
