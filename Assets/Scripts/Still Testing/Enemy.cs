db08d0c4fe5ad62d9a521d3fbbbb48252f597977
DESKTOP-89BTP30\Sam <samluxian@gmail.com>
2023-11-20 11:53:02 +0800

    敵人AI更新

--- Assets/Scripts/Still Testing/Enemy.cs
+++ Assets/Scripts/Still Testing/Enemy.cs
@@ -11,53 +11,144 @@ public class Enemy : MonoBehaviour
 
     public int maxHealth;
     int currentHealth;
+    public LayerMask playerLayer;
+    
+    public float detectionRange = 10f;
+    public float patrolRadius = 10f; 
+    private int currentPatrolIndex = 0;
+    public float chaseCooldown = 1.0f;
 
-    public NavMeshAgent agent;
+    private NavMeshAgent agent;
     private Transform targetPlayer;
+    private Vector3 patrolPoint;
 
-
-
+    private bool isPatrolling = true;
+    private bool isFacingRight = false; // Flag to track the enemy's facing direction
+    private float lastPlayerMovementTime = Mathf.NegativeInfinity;
 
     void Start()
     {
-        currentHealth = maxHealth;
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = 0f;
+        SetRandomPatrolPoint();
     }
 
     void Update()
     {
+
+        
+        if (isPatrolling)
+        {
+            Patrol();
+        }
+        else
+        {
+            if (Time.time - lastPlayerMovementTime > chaseCooldown)
+            {
+                FindNearestPlayer();
+                ChasePlayer();
+            }
+        }
+
+        if (targetPlayer == null)
+        {
+            // If there's no player, resume patrolling to a new random point
+            isPatrolling = true;
+        }
+    }
+
+    
+
+    void Patrol()
+    {
+        // Move the enemy towards the patrol point
+        agent.SetDestination(patrolPoint);
+
+        // If the enemy reaches the patrol point, set a new random point
+        if (Vector3.Distance(transform.position, patrolPoint) < 0.5f)
+        {
+            SetRandomPatrolPoint();
+        }
+
+        // Check for nearby players within the detection range
         FindNearestPlayer();
         if (targetPlayer != null)
         {
-            Chasing();
+            isPatrolling = false; // Stop patrolling when a player is detected
         }
-
     }
+
+    void SetRandomPatrolPoint()
+    {
+        float randomX = Random.Range(-patrolRadius, patrolRadius);
+        float randomZ = Random.Range(-patrolRadius, patrolRadius);
+        patrolPoint = transform.position + new Vector3(randomX, 0f, randomZ);
+    }
+
+
 
     void FindNearestPlayer()
     {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        Collider[] players = Physics.OverlapSphere(transform.position, detectionRange, playerLayer);
         float closestDistance = Mathf.Infinity;
+        Transform nearestPlayer = null;
 
-        foreach (GameObject player in players)
+        foreach (Collider playerCollider in players)
         {
-            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            float distanceToPlayer = Vector3.Distance(transform.position, playerCollider.transform.position);
             if (distanceToPlayer < closestDistance)
             {
                 closestDistance = distanceToPlayer;
-                targetPlayer = player.transform;
+                nearestPlayer = playerCollider.transform;
+            }
+        }
+
+        if (nearestPlayer != null)
+        {
+            SetTargetPlayer(nearestPlayer);
+            if (nearestPlayer.position.x > transform.position.x && !isFacingRight)
+            {
+                Flip();
+            }
+            else if (nearestPlayer.position.x < transform.position.x && isFacingRight)
+            {
+                Flip();
             }
         }
     }
 
-    private void Chasing()
+    void SetTargetPlayer(Transform player)
     {
-
-        agent.SetDestination(targetPlayer.position);
-        animator.SetFloat("Speed", 1);
-       
+        targetPlayer = player;
+        lastPlayerMovementTime = Time.time;
     }
 
-   
+    void ChasePlayer()
+    {
+        if (targetPlayer != null)
+        {
+            agent.stoppingDistance = 0.5f; // Set a stopping distance for chasing
+            agent.SetDestination(targetPlayer.position);
+        }
+    }
+
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, patrolRadius);
+    }
 
     public void TakeDamage(int damage)
     {
